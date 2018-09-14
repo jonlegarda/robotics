3958e5dc2557fac49c6b2f68f18f8138a7adfff7
jonlegarda <jonlegarda002@gmail.com>
2018-09-14 15:39:27 +0300

    Thursday, 14th September. More structure for the code.

--- hardware_package/scripts/move_robot.py
+++ hardware_package/scripts/move_robot.py
@@ -1,11 +1,12 @@
+import serial
 import rospy
 import hardware_package.communication_mainboard as cm
 from communication_mainboard.py import ComportMainboard
 
-   __name__ == '__main__':
-     	 mainboard_runner = ComportMainboard()
-         mainboard_runner.run()
-	 mainboard_runner.write("r/n")
-         mainboard_runner.close()
+if __name__ == '__main__':
+	mainboard_runner = ComportMainboard()
+	mainboard_runner.run()
+	mainboard_runner.write("r/n")
+       	 # mainboard_runner.close()
 
 
