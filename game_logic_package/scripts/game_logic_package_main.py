4d03c38ad6fd7ac58621011a3cad037ace7666a5
jonlegarda <jonlegarda002@gmail.com>
2018-09-22 19:37:17 +0300

    Saturday, 22nd September.

--- game_logic_package/scripts/game_logic_package_main.py
+++ game_logic_package/scripts/game_logic_package_main.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 from hardware_package.communication_mainboard import ComportMainboard
 from image_processing_package.msg import BallPoint
+from general_package.msg import MoveSpeed
 
 # class is for 
 
@@ -12,17 +13,39 @@ class GameLogic():
 	def __init__(self):
 		rospy.init_mode("game_logic_package", anonymous=True) #may be game_logic(?)
 		rospy.Subscriber("ballpoint_coordinates", BallPoint, self.callback)
+		self.ballSeen = False
+		rospy.Publisher("moving_speed", MoveSpeed, queue_size=10)
 
 	def callback(self, ballPoint):
 		print(str(ballPoint))
 		if (ballPoint.x < 380 and ballPoint.x > 260):
 			print("- STOP ROBOT! -")
+			self.speed_pub.publish(MoveSpeed(0,0,0,0))
+
+	def move_forward(speed):
+		return MoveSpeed(speed, (-1) * speed, 0, 0)
+
+	def move_backwards(speed):
+		return MoveSpeed((-1)*speed, speed, 0, 0)
+
+	def rotate(speed):
+		# rotates around its own axis
+		# speed: positive -> turn right; negative -> turn left
+		return MoveSpeed(speed, speed, speed, 0)
+
+	def circle(speed):
+		# rotates around axis in front of the robot
+		# speed: positive -> move left; negative -> move right
+		return Speeds(0, 0, speed, 0)
 
 if __name__ == '__main__':
 	try:
 		gameLogic = GameLogic()
 		rate = rospy.Rate(60)
 		while not rospy.is_shutdown():
+			if logic.ballSeen == False:
+				print("X - Ball not seen.")
+				logic.speed_pub.publish(rotate(-10))
 			rate.sleep()
 	except rospy.ROSInterruptException:
 		pass
