d45f6c818ef239360880ec78b02668ca76d90f2a
jonlegarda <jonlegarda002@gmail.com>
2018-09-22 20:50:51 +0300

    Saturday, 22nd September. Last of the day, detects green.

--- hardware_package/scripts/move_robot.py
+++ hardware_package/scripts/move_robot.py
@@ -1,38 +1,71 @@
 #! /usr/bin/env python
-
 import rospy
 from hardware_package.communication_mainboard import ComportMainboard
-from game_logic_package.msg import Twist
+from general.msg import MoveSpeed 
 
 class MainboardRunner():
 
     board = None
-    #launcherParams = Launcher()
+    def __init__(self):
+	rospy.init_node("connection_test", anonymous=True)
+        rospy.Subscriber("moving_speed", MoveSpeed, self.callback)
+	self.board = ComportMainboard()
 
-    #def launcher_params_callback(self, launcherMsg):
-    #  self.launcherParams = launcherMsg
-
-    def run(self):
-        rospy.init_node("comport_mainboad", anonymous=True)
+    #def run(self):
+        #rospy.init_node("comport_mainboad", anonymous=True)
         # rospy.Subscriber("hardware_module/launcher", Launcher, self.launcher_params_callback)
-        rospy.Subscriber("move_vector", Twist, twist_callback)
-        self.board = ComportMainboard()
-        self.board.run()
-
-        rate = rospy.Rate(60)
-        while not rospy.is_shutdown():
+        #rospy.Subscriber("move_vector", Twist, twist_callback)
+        #self.board = ComportMainboard()
+        #self.board.run()
+        #rate = rospy.Rate(60)
+        #while not rospy.is_shutdown():
         #self.sendSpeeds()
 	    #self.board.write("r/n")
 	    # This line below is supposed to make the robot move by activating 
 	    # a couple of wheels and make them move.
-	    self.board.write("sd:-20:20:0:0")
-        rate.sleep()
-        print "closing board"
-        self.board.close()
+	    #self.board.write("sd:-20:20:0:0")
+        #rate.sleep()
+        #print "closing board"
+        #self.board.close()
 
     #def sendSpeeds(self):
      #   self.board.servo(self.launcherParams.servoPos)
      #   self.board.launch_motor(self.launcherParams.motorSpeed)
+
+    def run(self):
+        self.board.run()
+	rospy.spin()
+	'''
+        r = rospy.Rate(30)
+        while not rospy.is_shutdown():
+            self.move_forward(10)
+            r.sleep()
+	'''
+        print("closing board")
+        self.board.close()
+
+    def speeds_callback(self, speeds):
+	print(str(speeds))
+	self.set_dir(speeds.left, speeds.right, speeds.back, speeds.thrower)
+
+    def move_forward(self, speed):
+        self.set_dir(speed, (-1) * speed, 0)
+
+    def move_backwards(self, speed):
+        self.set_dir((-1)*speed, speed, 0)
+
+    def rotate(self, speed):
+        self.set_dir(speed, speed, speed)
+
+    def circle(self, speed):
+        self.set_dir(0, 0, speed)
+
+    def set_dir(self, front_left, front_right, back, thrower=0):
+        self.board.write("sd:{}:{}:{}:{}".format(front_left, front_right, back, thrower))
+
+    def get_dir(self):
+        self.board.write('gs')
+        return self.board.readline()
 
 if __name__ == '__main__':
     try:
