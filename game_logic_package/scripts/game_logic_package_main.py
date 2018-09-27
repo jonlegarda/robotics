73e35fa4ab810c96a1fd7510d4c6a0315f20cb5c
jonlegarda <jonlegarda002@gmail.com>
2018-09-27 20:42:12 +0300

    Thursday, 27th of September. Drives to the ball in progress

--- game_logic_package/scripts/game_logic_package_main.py
+++ game_logic_package/scripts/game_logic_package_main.py
@@ -2,47 +2,111 @@
 import rospy
 import numpy as np
 import cv2
+import math
 from hardware_package.communication_mainboard import ComportMainboard
 from image_processing_package.msg import BallPoint
 from general_package.msg import MoveSpeed
 
+CENTER_POINT = 320
+BALL_LARGE = 30
+LEFT_PERMITTED_FOR_BALL = CENTER_POINT - BALL_LARGE
+RIGHT_PERMITTED_FOR_BALL = CENTER_POINT + BALL_LARGE
+
+BALL_ON_THE_RIGHT = "ball is on the right"
+BALL_ON_THE_LEFT = "ball is on the left"
+BALL_ON_CENTER = "ball is on the center"
+BALL_UNKNOWN = "ball is unknown"
+
+ROBOT_SPEED = 10
+
+WHEEL_LEFT_ANGLE = 240
+WHEEL_RIGHT_ANGLE = 120
+WHEEL_BACK_ANGLE = 0
+
+TASK_NUMBER = 1
+
 class GameLogic():
 
-	def __init__(self):
-		rospy.init_node("game_logic_package", anonymous=True) #may be game_logic(?)
-		rospy.Subscriber("ball_coordinates", BallPoint, self.callback)
-		self.ballSeen = False
-		self.speed_pub = rospy.Publisher("move_speed", MoveSpeed, queue_size=10)
+    def __init__(self):
+        rospy.init_node("game_logic_package", anonymous=True) #may be game_logic(?)
+        rospy.Subscriber("ball_coordinates", BallPoint, self.callback)
+        self.ballSeen = False
+        self.status = BALL_UNKNOWN
+        self.speed_pub = rospy.Publisher("move_speed", MoveSpeed, queue_size=10)
 
-	def callback(self, ballPoint):
-		print(str(ballPoint))
-		if (ballPoint.x < 350 and ballPoint.x > 290):
-			print("- STOP ROBOT! -")
-			self.speed_pub.publish(MoveSpeed(0,0,0,0))
-			self.ballSeen = True
+    def callback(self, ballPoint):
+        print(str(ballPoint))
+        # Try to be more precise and detect the ball in different parts of the frame.
+        if (ballPoint.x <= RIGHT_PERMITTED_FOR_BALL  and ballPoint.x >= LEFT_PERMITTED_FOR_BALL):
+            print("game_logic_package_main -> STOP ROBOT. BALL IS IN THE CENTER.")
+            self.speed_pub.publish(MoveSpeed(0,0,0,0))
+            self.ballSeen = True
+            self.status = BALL_ON_CENTER
+        elif (ballPoint.x > LEFT_PERMITTED_FOR_BALL):
+            print("game_logic_package_main -> Should rotate to the right.")
+            self.status = BALL_ON_THE_RIGHT
+        elif (ballPoint < RIGHT_PERMITTED_FOR_BALL):
+            print("game_logic_package_main -> Should rotate to the left")
+            self.status = BALL_ON_THE_LEFT
+        else:
+            print("game_logic_package_main -> Should rotate still rotate (no matter direction)")
+            self.status = BALL_UNKNOWN
 
 def move_forward(speed):
-	return MoveSpeed(speed, (-1) * speed, 0, 0)
+    return MoveSpeed(speed, (-1) * speed, 0, 0)
 
 def move_backwards(speed):
-	return MoveSpeed((-1)*speed, speed, 0, 0)
+    return MoveSpeed((-1)*speed, speed, 0, 0)
 
 def rotate(speed):
-	return MoveSpeed(speed, speed, speed, 10)
+    return MoveSpeed(speed, speed, speed, 10)
 
 def circle(speed):
-	return MoveSpeed(0, 0, speed, 0)
+    return MoveSpeed(0, 0, speed, 0)
+
+def calculate_speed(robotSpeed, robotDirectionAngle, wheelAngle):
+    return (robotSpeed * math.cos(math.radians(robotDirectionAngle - wheelAngle)))
 
 if __name__ == '__main__':
-	try:
-		gameLogic = GameLogic()
-		rate = rospy.Rate(60)
-		while not rospy.is_shutdown():
-			if not gameLogic.ballSeen:
-				print("X - Ball not seen.")
-				gameLogic.speed_pub.publish(rotate(-10))
-			else:
-				print("X - BALL SEEN")
-			rate.sleep()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        gameLogic = GameLogic()
+        rate = rospy.Rate(60)
+        while not rospy.is_shutdown():
+            if (TASK_NUMBER == 1):
+                if (gameLogic.status == BALL_ON_CENTER):
+                    # T1 - Robot moves side to side
+                    left_wheel = calculate_speed(ROBOT_SPEED, 90, WHEEL_LEFT_ANGLE)
+                    right_wheel = calculate_speed(ROBOT_SPEED, 90, WHEEL_RIGHT_ANGLE)
+                    back_wheel = calculate_speed(ROBOT_SPEED, 90, WHEEL_BACK_ANGLE)
+                    gameLogic.speed_pub.publish(MoveSpeed(left_wheel, right_wheel, back_wheel, 0))
+                    # gameLogic.speed_pub.publish(move_forward(6))
+                elif(gameLogic.status == BALL_ON_THE_LEFT):
+                    # T1 - Robot moves side to side
+                    left_wheel = calculate_speed(ROBOT_SPEED, 180, WHEEL_LEFT_ANGLE)
+                    right_wheel = calculate_speed(ROBOT_SPEED, 180, WHEEL_RIGHT_ANGLE)
+                    back_wheel = calculate_speed(ROBOT_SPEED, 180, WHEEL_BACK_ANGLE)
+                    gameLogic.speed_pub.publish(MoveSpeed(left_wheel, right_wheel, back_wheel, 0))
+                    # gameLogic.speed_pub.publish(rotate(7))
+                elif gameLogic.status == BALL_ON_THE_RIGHT:
+                    # T1 - Robot moves side to side
+                    left_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_LEFT_ANGLE)
+                    right_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_RIGHT_ANGLE)
+                    back_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_BACK_ANGLE)
+                    gameLogic.speed_pub.publish(MoveSpeed(left_wheel, right_wheel, back_wheel, 0))
+                    # gameLogic.speed_pub.publish(rotate(-7))
+                elif gameLogic.status == BALL_UNKNOWN:
+                    gameLogic.speed_pub.publish(rotate(-10))
+                rate.sleep()
+            else:
+                if (gameLogic.status != BALL_UNKNOWN):
+                    # T2 - The robot must go directly to the ball once ball is seen
+                    # NEEDS TO BE DONE!!
+                    left_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_LEFT_ANGLE)
+                    right_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_RIGHT_ANGLE)
+                    back_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_BACK_ANGLE)
+                    gameLogic.speed_pub.publish(MoveSpeed(left_wheel, right_wheel, back_wheel, 0))
+                else:
+                    gameLogic.speed_pub.publish(rotate(-10))
+                rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
