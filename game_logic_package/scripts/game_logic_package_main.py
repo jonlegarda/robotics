9bc2e3fd5f97570bf0e26c1223a779b58d8a8bcd
jonlegarda <jonlegarda002@gmail.com>
2018-09-28 11:16:58 +0300

    Friday, 28th September. First test of subtask

--- game_logic_package/scripts/game_logic_package_main.py
+++ game_logic_package/scripts/game_logic_package_main.py
@@ -7,22 +7,31 @@ from hardware_package.communication_mainboard import ComportMainboard
 from image_processing_package.msg import BallPoint
 from general_package.msg import MoveSpeed
 
+# Ball parameters position in a frame (maximum and minimum)
 CENTER_POINT = 320
 BALL_LARGE = 30
 LEFT_PERMITTED_FOR_BALL = CENTER_POINT - BALL_LARGE
 RIGHT_PERMITTED_FOR_BALL = CENTER_POINT + BALL_LARGE
 
+# Parameters that changes depending on the position of the ball.
 BALL_ON_THE_RIGHT = "ball is on the right"
 BALL_ON_THE_LEFT = "ball is on the left"
 BALL_ON_CENTER = "ball is on the center"
 BALL_UNKNOWN = "ball is unknown"
 
+BALL_CLOSE = "ball is close enough to robot."
+BALL_FAR = "ball is far away"
+BALL_DISTANCE_UNKNOWN = "ball is in an unknown position"
+
+# Robot default speed
 ROBOT_SPEED = 10
 
+# Angles of each wheel in order to use Omion
 WHEEL_LEFT_ANGLE = 240
 WHEEL_RIGHT_ANGLE = 120
 WHEEL_BACK_ANGLE = 0
 
+# Week 3: Two types of task for going through a ball.
 TASK_NUMBER = 1
 
 class GameLogic():
@@ -32,6 +41,7 @@ class GameLogic():
         rospy.Subscriber("ball_coordinates", BallPoint, self.callback)
         self.ballSeen = False
         self.status = BALL_UNKNOWN
+        self.ballDistance = BALL_DISTANCE_UNKNOWN
         self.speed_pub = rospy.Publisher("move_speed", MoveSpeed, queue_size=10)
 
     def callback(self, ballPoint):
@@ -52,6 +62,7 @@ class GameLogic():
             print("game_logic_package_main -> Should rotate still rotate (no matter direction)")
             self.status = BALL_UNKNOWN
 
+
 def move_forward(speed):
     return MoveSpeed(speed, (-1) * speed, 0, 0)
 
@@ -66,6 +77,9 @@ def circle(speed):
 
 def calculate_speed(robotSpeed, robotDirectionAngle, wheelAngle):
     return (robotSpeed * math.cos(math.radians(robotDirectionAngle - wheelAngle)))
+
+def calculate_robot_angle():
+    return 1
 
 if __name__ == '__main__':
     try:
@@ -101,6 +115,7 @@ if __name__ == '__main__':
                 if (gameLogic.status != BALL_UNKNOWN):
                     # T2 - The robot must go directly to the ball once ball is seen
                     # NEEDS TO BE DONE!!
+                    robot_angle = calculate_robot_angle()
                     left_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_LEFT_ANGLE)
                     right_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_RIGHT_ANGLE)
                     back_wheel = calculate_speed(ROBOT_SPEED, 0, WHEEL_BACK_ANGLE)
